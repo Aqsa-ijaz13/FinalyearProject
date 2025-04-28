5200b6e47e7b85b6510d50cd6e6e60cc46180c2e
Aqsa <iaqsa5107@gmail.com>
2025-04-28 12:52:01 +0500

    changings

--- lib/view/dailyaffairmation/DailyAffiarmation.dart
+++ lib/view/dailyaffairmation/DailyAffiarmation.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../utilis/Constants/app_colors.dart';
 import '../../utilis/Constants/app_images.dart';
-
-
 
 class DailyAffirmationScreen extends StatelessWidget {
   @override
@@ -38,47 +37,68 @@ class DailyAffirmationScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
               const SizedBox(height: 16),
-              GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16.0,
-                  mainAxisSpacing: 16.0,
-                ),
-                itemCount: affirmations.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(0.0),
-                      image: DecorationImage(
-                        image: AssetImage(affirmations[index]['image']!),
-                        fit: BoxFit.cover,
+
+              // Start StreamBuilder here
+              StreamBuilder<List<Map<String, String>>>(
+                stream: Stream.value(affirmations), // Simple static stream
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final data = snapshot.data!;
+                    return GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16.0,
+                        mainAxisSpacing: 16.0,
                       ),
-                    ),
-                    child: Container(
-                      alignment: Alignment.bottomCenter,
-                      decoration: BoxDecoration(
-                        color: Colors.black.withOpacity(0.4),
-                      ),
-                      child: Text(
-                        affirmations[index]['title']!,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.0,
-                          color: Colors.white,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  );
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () async {
+                            final url = data[index]['link']!;
+                            if (await canLaunchUrl(Uri.parse(url))) {
+                              await launchUrl(Uri.parse(url));
+                            } else {
+                              throw 'Could not launch $url';
+                            }
+                          },
+                          child: Container(
+                            width: 150,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(0.0),
+                              image: DecorationImage(
+                                image: AssetImage(data[index]['image']!),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            child: Container(
+                              alignment: Alignment.bottomCenter,
+                              decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.4),
+                              ),
+                              child: Text(
+                                data[index]['title']!,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16.0,
+                                  color: Colors.white,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
                 },
               ),
-              const SizedBox(height: 20),
-              // Add a button to navigate to EmergencyContactsScreen
 
+              const SizedBox(height: 20),
             ],
           ),
         ),
@@ -87,13 +107,46 @@ class DailyAffirmationScreen extends StatelessWidget {
   }
 }
 
+// Affirmations with images + links
 const affirmations = [
-  {'image': AppImages.mentalHealth, 'title': 'Mental Health'},
-  {'image': AppImages.positive, 'title': 'Positive Mindset'},
-  {'image': AppImages.miracle, 'title': 'Miracle Morning'},
-  {'image': AppImages.oldHand, 'title': 'Manifest'},
-  {'image': AppImages.lovePic, 'title': 'Love'},
-  {'image': AppImages.weightLose, 'title': 'Weight Loss'},
-  {'image': AppImages.self, 'title': 'Self-Esteem'},
-  {'image': AppImages.motivate, 'title': 'Motivation'},
+  {
+    'image': AppImages.mentalHealth,
+    'title': 'Mental Health',
+    'link': 'https://www.mentalhealthjournal.org/articles/mental-health-education-awareness-and-stigma-regarding-mental-illness-among-college-students.html'
+  },
+  {
+    'image': AppImages.positive,
+    'title': 'Positive Mindset',
+    'link': 'https://www.verywellmind.com/what-is-positive-thinking-2794772'
+  },
+  {
+    'image': AppImages.miracle,
+    'title': 'Miracle Morning',
+    'link': 'https://www.nbcnews.com/better/lifestyle/i-tried-miracle-morning-routine-month-here-s-what-happened-ncna981786'
+  },
+  {
+    'image': AppImages.oldHand,
+    'title': 'Manifest',
+    'link': 'https://www.theguardian.com/lifeandstyle/2022/feb/03/my-life-completely-turned-around-is-manifesting-the-key-to-happiness-or-wishful-thinking'
+  },
+  {
+    'image': AppImages.lovePic,
+    'title': 'Love',
+    'link': 'https://www.mqmentalhealth.org/how-can-love-affect-mental-health/'
+  },
+  {
+    'image': AppImages.weightLose,
+    'title': 'Weight Loss',
+    'link': 'https://www.healio.com/news/primary-care/20230622/mental-health-and-weight-loss-how-losing-weight-can-impact-mental-health' // Add your link here
+  },
+  {
+    'image': AppImages.self,
+    'title': 'Self-Esteem',
+    'link': 'https://pmc.ncbi.nlm.nih.gov/articles/PMC5747942/'
+  },
+  {
+    'image': AppImages.motivate,
+    'title': 'Motivation',
+    'link': 'https://thebigsilence.com/blogs/share-your-silence/how-your-mental-health-impacts-your-motivation?srsltid=AfmBOor5APM4VSmUsZBjDMcxM7iLYP8LtEwWIVirs7DwZenzvoWuozx7'
+  },
 ];
--- lib/view/talawat-e-quran/talwat-e-quran.dart
+++ lib/view/talawat-e-quran/talwat-e-quran.dart
@@ -11,53 +11,82 @@ class Surah {
   Surah({required this.title, required this.image, required this.audioUrl});
 }
 
-class TalwatEQuran extends StatelessWidget {
+class TalwatEQuran extends StatefulWidget {
+  @override
+  _TalwatEQuranState createState() => _TalwatEQuranState();
+}
+
+class _TalwatEQuranState extends State<TalwatEQuran> {
   final List<Surah> surahs = [
-
-    Surah(title: '99 names of Allah',
-        image: AppImages.pic4,
-        audioUrl: 'https://www.naataudio.com/UserFiles/1629399676/audio1629399906.mp3'),
-
-    Surah(
-      title: 'Surah Ad-Duha',
-      image: AppImages.pic6,
-      audioUrl: 'https://server8.mp3quran.net/afs/093.mp3',
-    ),
-    Surah(
-      title: 'Surah Ar-Rahman',
-      image: AppImages.pic2,
-      audioUrl: 'https://server8.mp3quran.net/afs/055.mp3',
-    ),
-    Surah(
-      title: 'Surah Al-Baqarah',
-      image: AppImages.pic3,
-      audioUrl: 'https://server8.mp3quran.net/afs/002.mp3',
-    ),
-    Surah(
-      title: 'Surah Al-Baqarah',
-      image: AppImages.pic4,
-      audioUrl: 'https://server8.mp3quran.net/afs/002.mp3',
-    ),
-    Surah(
-      title: 'Surah Quraysh',
-      image: AppImages.pic5,
-      audioUrl: 'https://server8.mp3quran.net/afs/106.mp3',
-    ),
-    Surah(
-      title: 'Surah Al-Asr',
-      image: AppImages.pic6,
-      audioUrl: 'https://server8.mp3quran.net/afs/103.mp3',
-    ),
-    Surah(
-      title: 'Surah Al-Ikhlas',
-      image: AppImages.pic7,
-      audioUrl: 'https://server8.mp3quran.net/afs/112.mp3',
-    ),
-    Surah(title: 'Surah Al-Fatiha',
-        image: AppImages.pic6,
-        audioUrl: 'https://server8.mp3quran.net/afs/001.mp3'),
-
+    Surah(title: '99 names of Allah', image: AppImages.pic4, audioUrl: 'https://www.naataudio.com/UserFiles/1629399676/audio1629399906.mp3'),
+    Surah(title: 'Surah Ad-Duha', image: AppImages.pic6, audioUrl: 'https://server8.mp3quran.net/afs/093.mp3'),
+    Surah(title: 'Surah Ar-Rahman', image: AppImages.pic2, audioUrl: 'https://server8.mp3quran.net/afs/055.mp3'),
+    Surah(title: 'Surah Al-Baqarah', image: AppImages.pic3, audioUrl: 'https://server8.mp3quran.net/afs/002.mp3'),
+    Surah(title: 'Surah Al-Baqarah', image: AppImages.pic4, audioUrl: 'https://server8.mp3quran.net/afs/002.mp3'),
+    Surah(title: 'Surah Quraysh', image: AppImages.pic5, audioUrl: 'https://server8.mp3quran.net/afs/106.mp3'),
+    Surah(title: 'Surah Al-Asr', image: AppImages.pic6, audioUrl: 'https://server8.mp3quran.net/afs/103.mp3'),
+    Surah(title: 'Surah Al-Ikhlas', image: AppImages.pic7, audioUrl: 'https://server8.mp3quran.net/afs/112.mp3'),
+    Surah(title: 'Surah Al-Fatiha', image: AppImages.pic6, audioUrl: 'https://server8.mp3quran.net/afs/001.mp3'),
+    Surah(title: 'Ayat-ul-kursi', image: AppImages.pic1, audioUrl: 'https://www.nooresunnat.com/Audio/Others/Complete%20Ayat%20ul%20Kursi%20Audio%20Recitation%20With%20Urdu%20Hindi%20Translation.mp3'),
   ];
+
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  Duration _duration = Duration.zero;
+  Duration _position = Duration.zero;
+  int? _currentlyPlayingIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer.setReleaseMode(ReleaseMode.loop);
+
+    _audioPlayer.onDurationChanged.listen((d) {
+      setState(() {
+        _duration = d;
+      });
+    });
+
+    _audioPlayer.onPositionChanged.listen((p) {
+      setState(() {
+        _position = p;
+      });
+    });
+
+    _audioPlayer.onPlayerComplete.listen((event) {
+      setState(() {
+        _position = Duration.zero;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  void _playPause(int index) async {
+    if (_currentlyPlayingIndex == index) {
+      if (_audioPlayer.state == PlayerState.playing) {
+        await _audioPlayer.pause();
+      } else {
+        await _audioPlayer.resume();
+      }
+    } else {
+      await _audioPlayer.stop();
+      await _audioPlayer.setSourceUrl(surahs[index].audioUrl);
+      await _audioPlayer.resume();
+      setState(() {
+        _currentlyPlayingIndex = index;
+      });
+    }
+  }
+
+  String formatTime(Duration d) {
+    final minutes = d.inMinutes.remainder(60).toString().padLeft(2, '0');
+    final seconds = d.inSeconds.remainder(60).toString().padLeft(2, '0');
+    return "$minutes:$seconds";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -80,117 +109,65 @@ class TalwatEQuran extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         itemCount: surahs.length,
         itemBuilder: (context, index) {
-          return SurahPlayerWidget(surah: surahs[index]);
+          final surah = surahs[index];
+          final isPlaying = _currentlyPlayingIndex == index && _audioPlayer.state == PlayerState.playing;
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            elevation: 4,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      surah.image,
+                      width: 150,
+                      height: 150,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    surah.title,
+                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  if (_currentlyPlayingIndex == index)
+                    Column(
+                      children: [
+                        Slider(
+                          min: 0,
+                          max: _duration.inSeconds > 0 ? _duration.inSeconds.toDouble() : 1,
+                          value: _position.inSeconds.toDouble().clamp(0, _duration.inSeconds > 0 ? _duration.inSeconds.toDouble() : 1),
+                          onChanged: (value) async {
+                            final position = Duration(seconds: value.toInt());
+                            await _audioPlayer.seek(position);
+                          },
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(formatTime(_position)),
+                            Text(formatTime(_duration - _position)),
+                          ],
+                        ),
+                      ],
+                    ),
+                  const SizedBox(height: 10),
+                  CircleAvatar(
+                    radius: 20,
+                    child: IconButton(
+                      icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+                      iconSize: 20,
+                      onPressed: () => _playPause(index),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
         },
-      ),
-    );
-  }
-}
-
-class SurahPlayerWidget extends StatefulWidget {
-  final Surah surah;
-  const SurahPlayerWidget({required this.surah});
-
-  @override
-  _SurahPlayerWidgetState createState() => _SurahPlayerWidgetState();
-}
-
-class _SurahPlayerWidgetState extends State<SurahPlayerWidget> {
-  final AudioPlayer _audioPlayer = AudioPlayer();
-  bool isPlaying = false;
-  Duration duration = Duration.zero;
-  Duration position = Duration.zero;
-
-  @override
-  void initState() {
-    super.initState();
-    _initAudio();
-  }
-
-  Future<void> _initAudio() async {
-    await _audioPlayer.setReleaseMode(ReleaseMode.loop);
-    await _audioPlayer.setSourceUrl(widget.surah.audioUrl);
-
-    _audioPlayer.onPlayerStateChanged.listen((state) {
-      setState(() => isPlaying = state == PlayerState.playing);
-    });
-
-    _audioPlayer.onDurationChanged.listen((d) {
-      setState(() => duration = d);
-    });
-
-    _audioPlayer.onPositionChanged.listen((p) {
-      setState(() => position = p);
-    });
-  }
-
-  String formatTime(Duration d) {
-    final minutes = d.inMinutes.remainder(60).toString().padLeft(2, '0');
-    final seconds = d.inSeconds.remainder(60).toString().padLeft(2, '0');
-    return "$minutes:$seconds";
-  }
-
-  @override
-  void dispose() {
-    _audioPlayer.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      elevation: 4,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.asset(
-                widget.surah.image,
-                width: 150,
-                height: 150,
-                fit: BoxFit.cover,
-              ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              widget.surah.title,
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Slider(
-              min: 0,
-              max: duration.inSeconds.toDouble(),
-              value: position.inSeconds.toDouble().clamp(0, duration.inSeconds.toDouble()),
-              onChanged: (value) async {
-                await _audioPlayer.seek(Duration(seconds: value.toInt()));
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(formatTime(position)),
-                Text(formatTime(duration - position)),
-              ],
-            ),
-            CircleAvatar(
-              radius: 20,
-              child: IconButton(
-                icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-                iconSize: 20,
-                onPressed: () async {
-                  if (isPlaying) {
-                    await _audioPlayer.pause();
-                  } else {
-                    await _audioPlayer.resume();
-                  }
-                },
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }

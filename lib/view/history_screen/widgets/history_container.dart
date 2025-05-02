a866247f6ef2fe13211d3f2bcb6fd7a859e0752c
Sulman Masood <sulmanmasood94@gmail.com>
2025-05-03 04:06:35 +0500

    updated code

--- lib/view/history_screen/widgets/history_container.dart
+++ lib/view/history_screen/widgets/history_container.dart
@@ -1,82 +1,137 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // For date formatting
 
 import '../../../utilis/Constants/app_texts.dart';
 import '../model/history_model/history_model.dart';
 
-Widget HistoryContainer(
-    {final String text = AppText.feelingToday,
-    final double height = 80,
-    //final double width = 290,
-    final String time = AppText.feelingToday,
-    final String mood = AppText.feelingToday,
-    final String image = AppText.feelingToday,
-    final Color color = const Color(0xffFFFFFF)}) {
-  final historyItem = HistoryDetail();
+class HistoryContainer extends StatelessWidget {
+  final String text;
+  final double height;
+  final String time;
+  final String mood;
+  final String image;
+  final Color color;
 
-  return Expanded(
-    child: ListView.builder(
-      itemCount: historyItem.Items.length,
-      // physics: AlwaysScrollableScrollPhysics(),
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 15,
-              ),
-              Container(
-                  height: height,
-                  //width: width,
-                  decoration: BoxDecoration(
-                      color: color,
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
+  // Constructor with named parameters
+  const HistoryContainer({
+    this.text = AppText.feelingToday,
+    this.height = 80,
+    this.time = AppText.feelingToday,
+    this.mood = AppText.feelingToday,
+    this.image = AppText.feelingToday,
+    this.color = const Color(0xffFFFFFF),
+  });
+
+  // Mapping of feelings to image paths
+  static const Map<String, String> feelingImageMap = {
+    'awful': 'assets/images/awful_logo.png',
+    'bad': 'assets/images/bad_logo.png',
+    'cry': 'assets/images/cry_logo.png',
+    'happy': 'assets/images/happy_logo.png',
+    'sad': 'assets/images/sad_logo.png',
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    final fireStore = FirebaseFirestore.instance.collection("history");
+    final auth = FirebaseAuth.instance.currentUser!.uid;
+
+    return StreamBuilder(
+      stream: fireStore.where('userId', isEqualTo: auth).snapshots(),
+      builder: (context, snapshot) {
+        return Expanded(
+          child: ListView.builder(
+            itemCount: snapshot.data?.docs.length ?? 0,
+            itemBuilder: (context, index) {
+              final colorValue = snapshot.data?.docs[index].data()?['color'];
+              final mycolor =
+                  colorValue is int ? Color(colorValue) : Colors.black;
+
+              final timestamp = snapshot.data?.docs[index]['createdAt'];
+              String dateStr = 'Unknown date';
+              String timeStr = 'Unknown time';
+
+              // Check if timestamp exists and is valid
+              if (timestamp != null && timestamp is Timestamp) {
+                final dateTime = timestamp.toDate();
+                dateStr = DateFormat('MMMM d').format(dateTime); // "May 13"
+                timeStr = TimeOfDay.fromDateTime(dateTime).format(context);
+              }
+
+              // Retrieve the feeling value
+              final feeling = snapshot.data?.docs[index]['feeling'] ?? 'Unknown';
+              // Get the corresponding image based on feeling
+              final imagePath = feelingImageMap[feeling.toLowerCase()] ?? 'assets/images/default_logo.png';
+
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      height: height,
+                      decoration: BoxDecoration(
+                        color: color,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
                             offset: Offset(0, 2),
                             spreadRadius: 0,
-                            blurRadius: 4)
-                      ]),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Image.asset(
-                        historyItem.Items[index].image,
-                        height: 40,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            historyItem.Items[index].date,
-                            style: TextStyle(
-                                fontFamily: "Inter",
-                                fontSize: 15,
-                                fontWeight: FontWeight.w400),
-                          ),
-                          Text(
-                            historyItem.Items[index].mood,
-                            style: TextStyle(
-                                fontFamily: "Inter",
-                                fontSize: 13,
-                                color: historyItem.Items[index].color,
-                                fontWeight: FontWeight.w700),
-                          ),
-                          Text(historyItem.Items[index].time),
+                            blurRadius: 4,
+                          )
                         ],
-                      )
-                    ],
-                  )),
-            ],
+                      ),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          // Display the image based on the feeling
+                          Image.asset(
+                            imagePath,
+                            height: 40,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                dateStr,
+                                style: TextStyle(
+                                  fontFamily: "Inter",
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              Text(
+                                feeling,
+                                style: TextStyle(
+                                  fontFamily: "Inter",
+                                  fontSize: 13,
+                                  color: mycolor,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              Text(timeStr),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
         );
       },
-    ),
-  );
+    );
+  }
 }
--- lib/view/home_screen/widgets/emoji_list.dart
+++ lib/view/home_screen/widgets/emoji_list.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '../model/emoji_model/emoji_model.dart';
 
@@ -14,27 +17,38 @@ Widget EmojiList({
       scrollDirection: Axis.horizontal,
       physics: AlwaysScrollableScrollPhysics(),
       itemBuilder: (context, index) {
-        return Container(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 9),
-                child: Image.asset(
-                  emojiItem.Items[index].image,
-                  height: 40,
+        final fireStore = FirebaseFirestore.instance.collection("history");
+        return GestureDetector(
+          onTap: () async {
+  await fireStore.add({
+    "feeling": emojiItem.Items[index].text,
+    'userId': FirebaseAuth.instance.currentUser!.uid,
+    'createdAt': Timestamp.now(),
+    'color': emojiItem.Items[index].color.value,
+  });
+},
+
+          child: Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 9),
+                  child: Image.asset(
+                    emojiItem.Items[index].image,
+                    height: 40,
+                  ),
                 ),
-              ),
-              Text(
-                emojiItem.Items[index].text,
-                style: TextStyle(
+                Text(
+                  emojiItem.Items[index].text,
+                  style: TextStyle(
                     fontFamily: "Inter",
                     fontSize: 14,
                     fontWeight: FontWeight.w700,
-                  color:  emojiItem.Items[index].color,
-
-                ),
-              )
-            ],
+                    color: emojiItem.Items[index].color,
+                  ),
+                )
+              ],
+            ),
           ),
         );
       },
